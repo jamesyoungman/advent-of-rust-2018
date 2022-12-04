0af2d9c72b8ac12280f05272a35f192894eed673
James Youngman <james@youngman.org>
2022-12-04 17:53:30 +0000

    Solved Day 01 part 2.

--- src/bin/day01/main.rs
+++ src/bin/day01/main.rs
@@ -1,11 +1,53 @@
+use std::collections::HashMap;
 use std::str;
 
-fn main() {
-    let input = str::from_utf8(include_bytes!("input.txt")).unwrap();
-    let numbers: Vec<i64> = input
-        .split_terminator('\n')
-        .map(|line| line.parse::<i64>())
+use lib::error::Fail;
+
+fn first_repeat(c: &[i64]) -> Option<i64> {
+    let mut seen: HashMap<i64, usize> = HashMap::new();
+    seen.insert(0, 1);
+    let mut current = 0;
+    for n in c.iter().cycle() {
+        current += n;
+        seen.entry(current)
+            .and_modify(|counter| *counter += 1)
+            .or_insert(1);
+        match seen.get(&current) {
+            Some(2) => {
+                return Some(current);
+            }
+            _ => (),
+        }
+    }
+    dbg!(&seen);
+    None
+}
+
+fn get_input(text: &str) -> Result<Vec<i64>, Fail> {
+    text.split_terminator('\n')
+        .map(|line| {
+            line.parse::<i64>()
+                .map_err(|e| Fail(format!("invalid input [{line}]: {e}")))
+        })
         .collect::<Result<Vec<i64>, _>>()
-        .expect("parse error");
-    println!("Day 01 part 1: {}", numbers.iter().sum::<i64>());
+}
+
+#[test]
+fn test_part2() {
+    assert_eq!(
+        first_repeat(&get_input("+1\n-1\n").expect("wanted valid input")),
+        Some(0)
+    );
+    assert_eq!(
+        first_repeat(&get_input("+3\n+3\n+4\n-2\n-4\n").expect("wanted valid input")),
+        Some(10)
+    );
+}
+
+fn main() {
+    let text = str::from_utf8(include_bytes!("input.txt")).unwrap();
+    let frequencies = get_input(&text).expect("wanted valid input");
+    let first_repeat: Option<i64> = first_repeat(&frequencies);
+    println!("Day 01 part 1: {}", frequencies.iter().sum::<i64>());
+    println!("Day 01 part 2: {}", first_repeat.expect("expected repeat"));
 }
