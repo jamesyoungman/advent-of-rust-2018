5889a30567046e6c5ba86f8a196a70375d2de69d
James Youngman <james@youngman.org>
2022-12-04 17:12:01 +0000

    Solved Day 01 part 1.

--- src/bin/day01/main.rs
+++ src/bin/day01/main.rs
@@ -1,1 +1,11 @@
-fn main() {}
+use std::str;
+
+fn main() {
+    let input = str::from_utf8(include_bytes!("input.txt")).unwrap();
+    let numbers: Vec<i64> = input
+        .split_terminator('\n')
+        .map(|line| line.parse::<i64>())
+        .collect::<Result<Vec<i64>, _>>()
+        .expect("parse error");
+    println!("Day 01 part 1: {}", numbers.iter().sum::<i64>());
+}
