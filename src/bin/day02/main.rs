7d834100fc2b3cb27a1cf7d480539deeae791b55
James Youngman <james@youngman.org>
2022-12-04 19:14:55 +0000

    Day 02: solved part 2.

--- src/bin/day02/main.rs
+++ src/bin/day02/main.rs
@@ -1,5 +1,8 @@
+use itertools::Itertools;
 use std::collections::HashMap;
 use std::str;
+
+use lib::error::Fail;
 
 fn letters_by_freq(s: &str) -> HashMap<usize, Vec<char>> {
     let mut freqs_by_letter: HashMap<char, usize> = HashMap::new();
@@ -37,26 +40,95 @@ fn test_two_or_three() {
     assert_eq!(two_or_three("ababab"), (None, Some(2)));
 }
 
-fn checksum(s: &str) -> usize {
-    let (count_two, count_three) = s
-        .split_terminator('\n')
-        .map(|line| two_or_three(line))
-        .fold((0, 0), |(total_two, total_three), (two, three)| {
+fn get_ids(s: &str) -> Vec<&str> {
+    s.split_terminator('\n').collect()
+}
+
+fn checksum(ids: &[&str]) -> usize {
+    let (count_two, count_three) = ids.iter().map(|line| two_or_three(line)).fold(
+        (0, 0),
+        |(total_two, total_three), (two, three)| {
             (
                 total_two + if two.is_some() { 1 } else { 0 },
                 total_three + if three.is_some() { 1 } else { 0 },
             )
-        });
+        },
+    );
     count_two * count_three
 }
 
 #[test]
 fn test_checksum() {
-    let cs = checksum("abcdef\nbababc\nabbcde\nabcccd\naabcdd\nabcdee\nababab\n");
-    assert_eq!(cs, 12);
+    let ids = get_ids("abcdef\nbababc\nabbcde\nabcccd\naabcdd\nabcdee\nababab\n");
+    assert_eq!(checksum(&ids), 12);
+}
+
+fn diffcount(left: &str, right: &str) -> usize {
+    left.chars()
+        .zip(right.chars())
+        .filter(|(l, r)| l != r)
+        .count()
+}
+
+#[test]
+fn test_diffcount() {
+    assert_eq!(diffcount("abcde", "axcye"), 2);
+    assert_eq!(diffcount("fghij", "fguij"), 1);
+}
+
+fn find_id_pair<'a, 'b>(ids: &'a [&'b str]) -> Option<(&'b str, &'b str)> {
+    for (left, right) in ids
+        .iter()
+        .cartesian_product(ids.iter())
+        .filter(|(l, r)| l != r)
+    {
+        if diffcount(left, right) == 1 {
+            return Some((left, right));
+        }
+    }
+    None
+}
+
+#[test]
+fn test_id_pair() {
+    let ids = vec![
+        "abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz",
+    ];
+    assert_eq!(find_id_pair(&ids), Some(("fghij", "fguij")));
+}
+
+fn solve_part2(ids: &[&str]) -> Result<String, Fail> {
+    match find_id_pair(&ids) {
+        None => Err(Fail("no suitable pair".to_string())),
+        Some((left, right)) => {
+            let rest: String = left
+                .chars()
+                .zip(right.chars())
+                .filter_map(|(l, r)| if l == r { Some(r) } else { None })
+                .collect();
+            Ok(rest)
+        }
+    }
+}
+
+#[test]
+fn test_solve_part2() {
+    let ids = vec![
+        "abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz",
+    ];
+    match solve_part2(&ids) {
+        Ok(s) if &s == "fgij" => (),
+        Ok(other) => {
+            panic!("wrong solution: {other}");
+        }
+        Err(e) => {
+            panic!("solution failed: {e}");
+        }
+    }
 }
 
 fn main() {
-    let text = str::from_utf8(include_bytes!("input.txt")).expect("valid input file");
-    println!("Day 02 part 1: {}", checksum(text));
+    let ids = get_ids(str::from_utf8(include_bytes!("input.txt")).expect("valid input file"));
+    println!("Day 02 part 1: {}", checksum(&ids));
+    println!("Day 02 part 2: {}", solve_part2(&ids).expect("solution"));
 }
